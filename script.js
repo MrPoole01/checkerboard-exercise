e01de2fde73fa155222e1423214d0a736e08b82d
Recardo Poole <recardo.poole@gmail.com>
2017-06-13 14:52:03 -0600

    Finished checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Your JS goes here
+var color1 = 0;
+var color2 = 255;
+
+function checkBoard() {
+  for (var i = 0; i < (window.innerHeight)/4; i++) {
+
+    var box = document.createElement('div')
+    document.body.appendChild(box)
+
+    box.style.width = '11.1%';
+    box.style.paddingBottom = '11.1%';
+    box.style.float = 'left';
+
+
+    if (i % 2 === 0) {
+      box.style.backgroundColor = 'rgba(13, '+ color1 + ', 12, 1)';
+    } else {
+      box.style.backgroundColor = 'rgba(120, 26,'+ color2 +' , 1)';
+    }
+    color1++
+    color2--
+  }
+}
+checkBoard()
